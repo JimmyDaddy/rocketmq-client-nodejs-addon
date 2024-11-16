9ba1d8d7ff56a5691a93caa73b675198806b48c0
noodanee <noodanee@qq.com>
2024-11-16 15:46:30 +0800

    chore: Update info

--- example/common.js
+++ example/common.js
@@ -3,4 +3,5 @@
 module.exports = {
     nameServer: '127.0.0.1:9876'
     // nameServer: 'onsaddr.cn-hangzhou.mq.aliyuncs.com:80'
+    // nameServer: 'v0-default-<instanceId>.mq.cloudrun.local:9515'
 };

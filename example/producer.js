6f2cbe91448ae87203a8cddb1ab2fdae2583c4c5
noodanee <noodanee@qq.com>
2025-05-13 18:19:28 +0800

    feat: refactor producer.js to use Promise.withResolvers for improved async handling

--- example/producer.js
+++ example/producer.js
@@ -1,41 +1,59 @@
 "use strict";
 
+if (typeof Promise.withResolvers === 'undefined') {
+    Promise.withResolvers = function () {
+        let resolve, reject
+        const promise = new Promise((res, rej) => {
+            resolve = res
+            reject = rej
+        })
+        return { promise, resolve, reject }
+    }
+}
+
+const readline = require('readline');
 const common = require('./common');
 const Producer = require('../').Producer;
-const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-void function () {
+void async function () {
     const producer = new Producer('GID_GROUP', {
         nameServer: common.nameServer,
     });
 
     // producer.setSessionCredentials('accessKey', 'secretKey', 'ALIYUN')
-    
-    producer.start(async function() {
-        console.log('producer started');
-        while(1) {
-            let resolve, reject;
-            const promise = new Promise((res, rej) => {
-                resolve = res;
-                reject = rej;
-              });
-            rl.question('Enter input: ', input => {
-                producer.send('TP_TOPIC', input, function(err, ret) {
-                    if (err) {
-                        console.error(err);
-                        reject(err)
-                    }
-                    console.log(ret);
-                    resolve(ret)
-                });
-            });
-            await promise.catch(console.error);
-        }
-    });
+
+    await producer.start();
+
+    while(1) {
+        const { promise, resolve, reject } = Promise.withResolvers()
+        rl.question('Enter input: ', async input => {
+            await producer.send('TP_TOPIC', input).catch(reject);
+            resolve();
+        });
+        await promise;
+    }
+
+    // producer.start(async function() {
+    //     console.log('producer started');
+    //     while(1) {
+    //         const { promise, resolve, reject } = Promise.withResolvers()
+    //         rl.question('Enter input: ', input => {
+    //             producer.send('TP_TOPIC', input, function(err, ret) {
+    //                 if (err) {
+    //                     console.error(err);
+    //                     reject(err)
+    //                 }
+    //                 console.log(ret);
+    //                 resolve(ret)
+    //             });
+    //         });
+    //         await promise.catch(console.error);
+    //     }
+    // });
 
 }();
